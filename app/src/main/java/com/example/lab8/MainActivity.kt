a4c75186d08a37a6e436eba38006e17d0b2fb853
Akimkhan Abat <Akim.khan0524@gmail.com>
2025-04-09 22:12:06 +0500

    Added BroadcastReceiver in MainActivity to receive data from RandomCharacterService

--- app/src/main/java/com/example/lab8/MainActivity.kt
+++ app/src/main/java/com/example/lab8/MainActivity.kt
@@ -1,20 +1,49 @@
 package com.example.lab8
 
+import android.content.*
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var randomCharacterEditText: EditText
+    private lateinit var serviceIntent: Intent
+    private val broadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            val data = intent?.getCharExtra("randomCharacter", '?') ?: '?'
+            randomCharacterEditText.setText(data.toString())
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        randomCharacterEditText = findViewById(R.id.editText_randomCharacter)
+        serviceIntent = Intent(this, RandomCharacterService::class.java)
+    }
+
+    fun onClick(view: View) {
+        when (view.id) {
+            R.id.button_start -> startService(serviceIntent)
+            R.id.button_end -> {
+                stopService(serviceIntent)
+                randomCharacterEditText.setText("")
+            }
+            R.id.button_next -> startActivity(Intent(this, MainActivity2::class.java))
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val filter = IntentFilter("my.custom.action.tag.lab6")
+        registerReceiver(broadcastReceiver, filter)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(broadcastReceiver)
     }
 }
